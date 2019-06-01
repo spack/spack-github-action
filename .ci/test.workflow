76eb1ad322463c350940c14ec47e08339727549f
arshul <arshulmohd123@gmail.com>
2019-06-01 23:06:01 +0530

    run with spack

--- .ci/test.workflow
+++ .ci/test.workflow
@@ -1,19 +1,18 @@
 workflow "spack tests" {
-  resolves = "run"
+  resolves = "run with spack"
 }
 
 action "lint" {
   uses = "actions/bin/shellcheck@master"
-  args = "-x run/entrypoint.sh scripts/spack_setup.sh "
+  args = "-x entrypoint.sh"
 }
 
-action "run" {
+action "run with spack" {
   needs = "lint"
-  uses = "./run"
-  args = ["lulesh2.0", "-s", "100", "-i", "10"]
+  uses = "./"
+  args = "install lulesh~mpi"
   env = {
-      SPACK_GIT_URL = "https://github.com/spack/spack"
-      SPACK_GIT_REF = "develop"
-      SPACK_SPEC = "lulesh~mpi"
+      SPACK_RESET_CONFIG="true"
+      SPACK_GIT_REF ="develop"
   }
 }
---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -ex
+
+if [[ -z "$*" ]]; then
+  echo "Expecting one or more arguments"
+  exit 1
+fi
+
+#remove spack if exist previously
+if [[   -d "$GITHUB_WORKSPACE"/temp/spack ]]; then
+    rm -rf "$GITHUB_WORKSPACE"/temp
+fi
+
+# install Spack
+git clone https://github.com/spack/spack  "$GITHUB_WORKSPACE"/temp/spack&& \
+    PATH=$PATH:"$GITHUB_WORKSPACE"/temp/spack/bin && \
+    echo "$PATH" && export PATH && \
+    bash "$GITHUB_WORKSPACE"/temp/spack/share/spack/setup-env.sh
+
+if [[ -n "$SPACK_RESET_CONFIG" ]]; then
+  if [[ -z "$SPACK_GIT_REF" ]]; then
+    echo "Please provide the git ref to reset"
+    exit 1
+  else
+    git -C "$GITHUB_WORKSPACE"/temp/spack fetch
+    git -C "$GITHUB_WORKSPACE"/temp/spack reset --hard "$SPACK_GIT_REF"
+  fi
+fi
+
+# run commands
+spack "$@"
